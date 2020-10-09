5e25ac92fb8b51074d47ec85ecf3efeb16f19339
jpassgo <jeffpascoe9@gmail.com>
2020-10-08 20:48:02 -0500

    Adding Sedan and Truck, implementations of Vehicle Abstract Class.

---
+++ creational/factory/Sedan.py
@@ -0,0 +1,24 @@
+from Vehicle import Vehicle
+
+
+class Sedan(Vehicle):
+
+    def start_engine(self):
+        pass
+
+    def kill_engine(self):
+        pass
+
+    def drive(self):
+        pass
+
+    def open_door(self):
+        pass
+
+    def turn(self, direction):
+        pass 
+
+    def open_trunk(self, direction):
+        pass
+
+
---
+++ creational/factory/Truck.py
@@ -0,0 +1,30 @@
+from Vehicle import Vehicle
+
+
+class Truck(Vehicle):
+
+    def start_engine(self):
+        pass
+
+    def kill_engine(self):
+        pass
+
+    def drive(self):
+        pass
+
+    def open_door(self):
+        pass
+
+    def turn(self, direction):
+        pass 
+
+    def open_truck_bed(self, direction):
+        pass
+
+    def tow_load(self, load_weight):
+        pass
+
+    def engage_four_wheel_drive(self):
+        pass
+
+    
